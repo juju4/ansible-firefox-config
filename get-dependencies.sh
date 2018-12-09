2fbe261139ddb2accf7fe778737329a75d88845e
juju4 <juju4@users.noreply.github.com>
2018-12-09 13:13:26 -0500

    galaxy naming + lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,11 +9,10 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+#[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
-#[ ! -e $rolesdir/juju4.firefox-config ] && ln -s ansible-firefox-config $rolesdir/juju4.firefox-config
-[ ! -e $rolesdir/juju4.firefox-config ] && cp -R $rolesdir/ansible-firefox-config $rolesdir/juju4.firefox-config
+#[ ! -e $rolesdir/juju4.firefox_config ] && ln -s ansible-firefox-config $rolesdir/juju4.firefox_config
+[ ! -e $rolesdir/juju4.firefox_config ] && cp -R $rolesdir/ansible-firefox-config $rolesdir/juju4.firefox_config
 
 ## don't stop build on this script return code
 true
-
