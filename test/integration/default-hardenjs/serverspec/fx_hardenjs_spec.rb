a4d390b8258cb994b6824b11c481029eadc6398e
juju4 <juju4@users.noreply.github.com>
2019-04-06 12:26:56 -0400

    test hardenjs: remove hash check - depends on variables

--- test/integration/default-hardenjs/serverspec/fx_hardenjs_spec.rb
+++ test/integration/default-hardenjs/serverspec/fx_hardenjs_spec.rb
@@ -14,7 +14,6 @@ fx_userjs = '/etc/firefox/firefox.js'
 describe file("#{fx_userjs}-pyllyukko") do
   it { should be_file }
   it { should be_mode '644' }
-  its(:sha256sum) { should eq '330c54106c483a89167a90f0a6d3442b83f3407af000e632bdef6f105c5f2596' }
 end
 describe file("#{fx_userjs}") do
   it { should be_file }
