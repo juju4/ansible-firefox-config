3434b707d8617364a03897dc68363cca75a3c82d
juju4 <juju4@users.noreply.github.com>
2019-04-06 12:13:04 -0400

    test hardenjs: fix filename

--- test/integration/default-hardenjs/serverspec/fx_hardenjs_spec.rb
+++ test/integration/default-hardenjs/serverspec/fx_hardenjs_spec.rb
@@ -11,7 +11,7 @@ set :backend, :exec
 
 fx_userjs = '/etc/firefox/firefox.js'
 
-describe file("#{fx_userjs}.orig") do
+describe file("#{fx_userjs}-pyllyukko") do
   it { should be_file }
   it { should be_mode '644' }
   its(:sha256sum) { should eq '330c54106c483a89167a90f0a6d3442b83f3407af000e632bdef6f105c5f2596' }
